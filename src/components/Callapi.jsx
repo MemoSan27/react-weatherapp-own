ba06741e36ac86076b0d30ab999fa34aac984c03
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-10-26 14:43:59 -0700

    span color modified

--- src/components/Callapi.jsx
+++ src/components/Callapi.jsx
@@ -5,14 +5,26 @@ import Weathercard from './Weathercard';
 const CallApi = () => {
 
     const [ city, setCity ] = useState();
-    const [ inputValue, setInputValue] = useState('pamplona');
+    const [ inputValue, setInputValue] = useState('mazatlan');
+    const [ hasError, setHasError ] = useState(false);
+    const [isLoading, setsIsLoading] = useState(true);
     const id = 'f1be5c352ab1ed63c855e6f5c16d3a5c';
 
     useEffect( () => {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${ inputValue }&appid=${ id }`;
+        setsIsLoading(true)
         axios.get(url)
-            .then( res => setCity(res.data))
-            .catch( err => console.log(err))
+            .then( res => {
+                setCity(res.data)
+                setHasError(false)
+            })
+            .catch( err => {
+                console.log(err)
+                setHasError(true)
+            })
+            .finally( () => {
+                setsIsLoading(false)
+            })
     }, [inputValue]);
 
     const inputCity = useRef();
@@ -28,9 +40,19 @@ const CallApi = () => {
             <form onSubmit={handleSubmit}>
                 <input ref={inputCity} className='card__input' type='text' placeholder='Name of your city...' />
             </form>
-            <Weathercard 
-                city={city}
-            />
+            {
+                isLoading
+                ? <h2 className='loading'> Loading....</h2>
+                : (
+                    hasError
+                    ? (<h2 className='error'> La ciudad de <span className='error__city'> {inputValue} </span>  no existe en nuestra base de datos</h2>)
+                    :(
+                        <Weathercard 
+                            city={city}
+                        />
+                    )
+                )
+            }
         </div>
     </main>
   )
