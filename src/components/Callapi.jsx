8e47464c88cf73bf6575f4c48208167c97a1f1b9
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-10-27 06:53:37 -0700

    changes added sucess

--- src/components/Callapi.jsx
+++ src/components/Callapi.jsx
@@ -4,27 +4,63 @@ import Weathercard from './Weathercard';
 
 const CallApi = () => {
 
+    const [coords, setCoords] = useState();
     const [ city, setCity ] = useState();
-    const [ inputValue, setInputValue] = useState('mazatlan');
+    const [ inputValue, setInputValue] = useState();
     const [ hasError, setHasError ] = useState(false);
     const [isLoading, setsIsLoading] = useState(true);
     const id = 'f1be5c352ab1ed63c855e6f5c16d3a5c';
 
+    const success = (position) => {
+        const obj = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      }
+      setCoords(obj);
+    }
+    
     useEffect( () => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ inputValue }&appid=${ id }`;
-        setsIsLoading(true)
+      setsIsLoading(true)
+      navigator.geolocation.getCurrentPosition(success)
+    }, []);
+  
+    
+    useEffect( () => {
+      if(coords && !city){
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${ coords.lat }&lon=${ coords.lon }&appid=${ id }`
         axios.get(url)
-            .then( res => {
-                setCity(res.data)
-                setHasError(false)
-            })
-            .catch( err => {
-                console.log(err)
-                setHasError(true)
-            })
-            .finally( () => {
-                setsIsLoading(false)
-            })
+          .then( res => {
+            /* const celsius = (res.data.main.temp - 273.15).toFixed(1)
+            const farenheit = (celsius * 9/5 + 32).toFixed(1)
+            setTemp({ celsius, farenheit }) */
+            setCity(res.data)
+          })
+          .catch( (err) => {
+            console.log(err);
+          })
+          .finally( () => {
+            setsIsLoading(false);
+          })
+      }
+    }, [coords])
+
+    useEffect( () => {
+        if(city){
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${ inputValue }&appid=${ id }`;
+            setsIsLoading(true)
+            axios.get(url)
+                .then( res => {
+                    setCity(res.data)
+                    setHasError(false)
+                })
+                .catch( err => {
+                    console.log(err)
+                    setHasError(true)
+                })
+                .finally( () => {
+                    setsIsLoading(false)
+                })
+        }
     }, [inputValue]);
 
     const inputCity = useRef();
--- src/components/Weathercard.jsx
+++ src/components/Weathercard.jsx
@@ -1,15 +1,18 @@
 
 const Weathercard = ( { city } ) => {
 
-    const icon = city?.weather?.[0].icon + ".gif";
+    const icon = city?.weather?.[0].icon.charAt(0);
+    const icon2 = city?.weather?.[0].icon.charAt(1);
+    const iconDef = `${icon}${icon2}.gif`;
     
+    console.log(city);
 
    return (
         <>
             <article>
                 <div className="card__info">
                     <h2 className="card__name"> Clima actual en <span> { city?.name }, {city?.sys?.country} </span>  </h2>
-                    <img className="card__gif" src={icon} alt="Img Sun" />
+                    <img className="card__gif" src={iconDef} alt="Img Sun" />
                     <h2 className="card__temp"> 
                         <span className="card__grados-desc"> Temperatura actual: </span> 
                         <span className="card__grados"> { Math.round(city?.main?.temp - 273.15) }°C </span>
